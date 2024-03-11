251ac4808288c382150423dd88a8a01a598241fe
dais-k <dqm11314@nifty.com>
2024-03-12 01:50:02 +0900

    戦闘詳細に特殊マス(気球マス、環礁マス)の表示追加

--- ElectronicObserver/Data/Battle/Detail/BattleDetailDescriptor.cs
+++ ElectronicObserver/Data/Battle/Detail/BattleDetailDescriptor.cs
@@ -324,8 +324,22 @@ namespace ElectronicObserver.Data.Battle.Detail
 							sb.AppendFormat("煙幕展開: {0}重\r\n", p.SmokeCount);
 						}
 
-						sb.AppendLine();
+						if (p.BalloonCell != 0)
+						{
+							sb.AppendLine("阻塞気球対象マス");
+						}
 
+						if (p.AtollCell != 0)
+						{
+							sb.AppendLine("環礁マス(開幕雷撃不可)");
+						}
+
+						if (p.Xal01Cell != 0)
+						{
+							sb.AppendLine("装甲破砕済み");
+						}
+
+						sb.AppendLine();
 						break;
 
 					case PhaseSupport p:
