3fd79b52fabd6b1fa7349a7e67c29f0bb3fb0d32
dais-k <dqm11314@nifty.com>
2025-03-17 08:27:28 +0900

    20250317

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -31,12 +31,12 @@ namespace ElectronicObserver.Utility
 		/// <summary>
 		/// バージョン(英語)
 		/// </summary>
-		public static string VersionEnglish => "4.7.1";
-
+		public static string VersionEnglish => "4.7.1 20250317";
+		
 		/// <summary>
 		/// 更新日時
 		/// </summary>
-		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2025/03/16 08:00:00");
+		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2025/03/17 07:00:00");
 		
 		private static System.Net.WebClient client;
 		private static readonly Uri uri = new("https://raw.githubusercontent.com/dais-k/ElectronicObserver/develop/ElectronicObserver/version.txt");
