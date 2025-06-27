3fa3b9340cbcd17413af74970191cd3f7692a4c5
dais-k <dqm11314@nifty.com>
2025-06-28 02:01:29 +0900

    全翼型戦闘爆撃機の装備アイコン追加

--- ElectronicObserver/Resource/ResourceManager.cs
+++ ElectronicObserver/Resource/ResourceManager.cs
@@ -234,6 +234,7 @@ namespace ElectronicObserver.Resource
 			JetInterceptor,
 			InterceptorShinden,
 			NightBomber,
+			FlyingWingJetBomber,
 			Locked,					
 			Unknown,				//不明な場合はこれ
 		}
@@ -496,6 +497,7 @@ namespace ElectronicObserver.Resource
 				LoadImageFromArchive(Equipments, archive, mstpath + @"Equipment/JetInterceptor.png", "Equipment_JetInterceptor");
 				LoadImageFromArchive(Equipments, archive, mstpath + @"Equipment/InterceptorShinden.png", "Equipment_InterceptorShinden");
 				LoadImageFromArchive(Equipments, archive, mstpath + @"Equipment/NightBomber.png", "Equipment_NightBomber");
+				LoadImageFromArchive(Equipments, archive, mstpath + @"Equipment/FlyingWingJetBomber.png", "Equipment_FlyingWingJetBomber"); 
 				LoadImageFromArchive(Equipments, archive, mstpath + @"Equipment/Locked.png", "Equipment_Locked");
 				LoadImageFromArchive(Equipments, archive, mstpath + @"Equipment/Unknown.png", "Equipment_Unknown");
 			}
