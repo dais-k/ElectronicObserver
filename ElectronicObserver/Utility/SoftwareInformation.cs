0f99c31a590d65c3932dc5fe83879b0cc582b7fd
dais-k <dqm11314@nifty.com>
2025-10-18 04:29:53 +0900

    20251018

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -31,12 +31,12 @@ namespace ElectronicObserver.Utility
 		/// <summary>
 		/// バージョン(英語)
 		/// </summary>
-		public static string VersionEnglish => "4.7.1 20250928";
+		public static string VersionEnglish => "4.7.1 20251018";
 		
 		/// <summary>
 		/// 更新日時
 		/// </summary>
-		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2025/09/28 00:00:00");
+		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2025/10/18 00:00:00");
 		
 		private static System.Net.WebClient client;
 		private static readonly Uri uri = new("https://raw.githubusercontent.com/dais-k/ElectronicObserver/develop/ElectronicObserver/version.txt");
